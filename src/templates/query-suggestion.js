73b1fc306d6c3aca2a6fc892ca5785e15f16f9ec
Mike Cronin <mikec1589@gmail.com>
2021-11-04 12:15:27 -0500

    Updates

--- src/templates/query-suggestion.js
+++
@@ -1,5 +0,0 @@
-const querySuggestionTemplate = hit => `<ul class="autofill-list">
-	<li class="autofill-text">${hit._highlightResult.name.value}</li>
-  </ul>`;
-
-export default querySuggestionTemplate;
